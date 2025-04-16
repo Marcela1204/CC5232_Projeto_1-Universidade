c9cbb513a0e322a69ad678b091116d862297def4
@Marcelinha <131552194+Marcela1204@users.noreply.github.com>
2025-04-16 02:27:52 -0300

    Update query8.sql

--- queries/query8.sql
+++ queries/query8.sql
@@ -1,6 +1,5 @@
 -- Encontre os nomes de todos os estudantes que cursaram "Banco de Dados" (course_id = 'CS-101').
-SELECT DISTINCT a.nome, a.ra
+SELECT a.nome
 FROM historico_escolar h
-JOIN alunos a ON h.ra = a.ra
-JOIN disciplinas_lecionadas d ON h.disciplina = d.disciplina
-WHERE d.disciplina = 'Banco de Dados';
+JOIN alunos a ON h.RA = a.RA
+WHERE h.disciplina = 'Banco de Dados';
